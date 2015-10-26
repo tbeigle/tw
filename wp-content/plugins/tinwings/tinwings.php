96d772d69e841f539fdc23ec719157c96b5d2811
Tom Beigle <tom@designated-developers.com>
2015-10-26 09:38:12 -0500

    TW-9: Saving recent changes. Ultimately, a new plugin for DD Clover will need to be added.

--- wp-content/plugins/tinwings/tinwings.php
+++ wp-content/plugins/tinwings/tinwings.php
@@ -88,15 +88,14 @@ if (!class_exists( 'TinWingsHelper')) {
   }
 }
 
-if (!class_exists( 'TinWingsClover')) {
+// TO DO: Create a different plugin for this, one that can be reused for any site.
+if (!class_exists( 'TinWingsClover' )) {
   class TinWingsClover {
-    // Vars
-    //private $client_id = 'XBV2A5BTAEPDM';
-    //private $client_secret = '7f663c3f-d438-ff62-a636-660199c15a24';
-    private $client_id = 'NJASA1B5VA0MP';
-    private $client_secret = '3e32545e-6a10-de95-6219-af52539ab3fc';
-    var $access_token;
+    //var $client_id = 'XBV2A5BTAEPDM';
+    var $client_id = 'NJASA1B5VA0MP';
     var $merchant_id;
+    var $uid;
+    var $mail = '';
     private $valid = FALSE;
     
     /**
@@ -120,62 +119,25 @@ if (!class_exists( 'TinWingsClover')) {
      */
     public function good_to_go() {
       $opt = get_option('tw_clover', array(
-        'access_token' => '',
         'merchant_id' => '',
+        'uid' => '',
+        'mail' => '',
       ));
       
-      $this->access_token = $opt['access_token'];
       $this->merchant_id = $opt['merchant_id'];
+      $this->uid = $opt['uid'];
+      $this->mail = isset($opt['mail']) ? $opt['mail'] : '';
       
-      return !empty($this->access_token) && !empty($this->merchant_id) && !empty($this->client_id) && !empty($this->client_secret);
+      return !empty($this->merchant_id) && !empty($this->uid);
     }
     
     /**
      * Prints a link for fetching an oauth code.
      */
     public function get_code() {
-      echo '<a href="https://www.clover.com/oauth/authorize?client_id=' . $this->client_id . '&redirect_uri=' . get_site_url() . '/twclover/auth">';
+      echo '<a href="https://www.clover.com/oauth/authorize?client_id=' . $this->client_id . '&redirect_uri=https://clover.designated-developers.com/ddclover/setup">';
       echo 'Authorize the App';
       echo '</a>';
-    }
-    
-    /**
-     * Retrieves an oauth token.
-     */
-    public function authorize($code) {
-      $url = 'https://www.clover.com/oauth/token?client_id=' . $this->client_id . '&client_secret=' . $this->client_secret . '&code=' . $code;
-      
-      $response = wp_remote_get($url);
-      
-      if (!is_array($response)) {
-        echo '<pre>' . print_r($response) . '</pre>';
-      }
-      elseif (!empty($response['body'])) {
-        $decoded = json_decode($response['body']);
-        
-        $opt = get_option('tw_clover', array(
-          'access_token' => '',
-          'merchant_id' => '',
-        ));
-        
-        if (!empty($decoded->access_token)) {
-          $opt['access_token'] = $decoded->access_token;
-        }
-        
-        update_option('tw_clover', $opt);
-      }
-      
-      // To make a call using the token (from command line, example for orders request)
-      // curl -s https://api.clover.com/v3/merchants/[Merchant ID]/orders --header "Authorization:Bearer [API Token]" | python -mjson.tool
-    }
-    
-    /**
-     * Runs on onchangeapi action
-     */
-    public function __invoke() {
-      if ($this->valid) {
-        // Do yo shiii
-      }
     }
     
     /**
@@ -184,15 +146,6 @@ if (!class_exists( 'TinWingsClover')) {
      */
     private function validatePostData(array $postdata) {
       return TRUE;
-    }
-    
-    /**
-     * Registers the path for the Clover webhook listener.
-     */
-    public function addListener() {
-      /*global $wp;
-      $wp->add_query_var('args');
-      add_rewrite_rule('twclover\/(.*)','index.php?pagename=clovertestapp&args=$matches[1]','top');*/
     }
   }
 }
@@ -207,8 +160,7 @@ add_action('init', 'tinwings_register_extra_pages');
 
 
 function tinwings_register_extra_pages() {
-  add_feed('twclover', 'tinwings_clover_oauth_auth');
-  add_feed('twclover/auth', 'tinwings_clover_oauth_receive');
+  add_feed('twclover', 'tinwings_clover_auth');
 }
 
 function tinwings_register_settings() {
@@ -216,31 +168,26 @@ function tinwings_register_settings() {
   
   if (get_option('tw_clover') === FALSE) {
     update_option('tw_clover', array(
-      'access_token' => '',
       'merchant_id' => '',
+      'uid' => '',
+      'mail' => '',
     ));
   }
 }
 
-function tinwings_clover_oauth_auth() {
-  print '<p>It works!</p>';
-}
-
-function tinwings_clover_oauth_receive() {
-  if (!empty($_GET['code'])) {
+function tinwings_clover_auth() {
+  if (!empty($_GET['ddcm']) && !empty($_GET['ddcu'])) {
     $opt = get_option('tw_clover', array(
-      'access_token' => '',
-      'merchant_id' => '',
+      'merchant_id' => 0,
+      'uid' => 0,
+      'mail' => '',
     ));
     
-    if (!empty($_GET['merchant_id'])) {
-      $opt['merchant_id'] = $_GET['merchant_id'];
-    }
-    
-    update_option('tw_clover', $opt);
-    
-    $twc = new TinWingsClover();
-    $twc->authorize($_GET['code']);
+    update_option('tw_clover', array(
+      'merchant_id' => $_GET['ddcm'],
+      'uid' => $_GET['ddcu'],
+      'mail' => !empty($opt['mail']) ? $opt['mail'] : '',
+    ));
     
     wp_redirect('/wp-admin/options-general.php?page=tinwings-clover-options');
     exit();
@@ -271,10 +218,16 @@ function tinwings_clover_options() {
     $twc->get_code();
   }
   else {
-    echo '<h2>Clover Settings</h2>';
+    echo '<h2>Designated Developers Clover Settings</h2>';
     echo '<form method="post" action="options.php">';
-    echo '<div><label>Merchant ID</label><input type="text" name="tw_clover[merchant_id]" value="' . $twc->merchant_id . '"></div>';
-    echo '<div><label>Access Token</label><input type="text" name="tw_clover[access_token]" value="' . $twc->access_token . '"></div>';
+    settings_fields('tinwings_clover');
+    $options = get_option('tw_clover');
+    echo '<div><label>Merchant ID</label>: ' . $twc->merchant_id . '</div>';
+    echo '<div><label>DD Clover User ID</label>: ' . $twc->uid . '</div>';
+    echo '<div><label>DD Clover User Email</label><input type="text" name="tw_clover[mail]" value="' . $twc->mail . '"></div>';
+    echo '<input type="hidden" name="tw_clover[merchant_id]" value="' . $twc->merchant_id . '">';
+    echo '<input type="hidden" name="tw_clover[uid]" value="' . $twc->uid . '">';
+    
     submit_button();
     echo '</form>';
   }
