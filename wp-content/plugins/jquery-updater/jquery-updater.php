80a4dff5b133a3ce8959eaab978cb6bef306c844
Juan Timana <juan.timana@planleft.com>
2016-05-24 12:41:03 -0500

    PESP-1809: Updated jQuery Updater

--- wp-content/plugins/jquery-updater/jquery-updater.php
+++ wp-content/plugins/jquery-updater/jquery-updater.php
@@ -3,7 +3,7 @@
  * Plugin Name: jQuery Updater
  * Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
  * Description: This plugin updates jQuery to the latest  stable version.
- * Version: 2.2.3
+ * Version: 2.2.4
  * Author: Ramoonus
  * Author URI: http://www.ramoonus.nl/
  * License: GPL3
@@ -22,13 +22,13 @@ function rw_jquery_updater() {
 	// Deregister core jQuery
 	wp_deregister_script('jquery');
 	// Register
-	wp_enqueue_script('jquery', plugins_url('/js/jquery-2.2.3.min.js', __FILE__), false, '2.2.3');
+	wp_enqueue_script('jquery', plugins_url('/js/jquery-2.2.4.min.js', __FILE__), false, '2.2.4');
 
 	// jQuery Migrate
 	// Deregister core jQuery Migrate
 	wp_deregister_script('jquery-migrate');
 	// Register
-    wp_enqueue_script('jquery-migrate', plugins_url('/js/jquery-migrate-1.3.0.min.js', __FILE__), array('jquery'), '1.3.0'); // require jquery, as loaded above
+    wp_enqueue_script('jquery-migrate', plugins_url('/js/jquery-migrate-1.4.1.min.js', __FILE__), array('jquery'), '1.4.1'); // require jquery, as loaded above
 }
 // Front-End
 add_action('wp_enqueue_scripts', 'rw_jquery_updater');
