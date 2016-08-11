5d57d98cd0b0e8089fb56d4c2039ffc5b8eb99a4
Juan Timana <juan.timana@planleft.com>
2016-08-11 17:18:34 -0500

    TINWIN-42: Updating working hours

---
+++ wp-content/themes/tw/footer.php
@@ -0,0 +1,34 @@
+<div class="footer-box footer-box1">
+<img src="<?php bloginfo('template_url'); ?>/images/tin-wings-market.png" class="tin-wings-market" />
+<p>
+816 51st Avenue North<br/>
+Nashville, Tennessee</p>
+
+<p><strong>Online Market Hours:</strong> Wednesday 10 am - Sunday 10 am<br>
+<strong>Tinwings Walk-in Market:</strong> Tuesday - Friday 11 am - 6 pm</p>
+
+<img src="<?php bloginfo('template_url'); ?>/images/lets-talk2.png" class="lets-talk2" />
+
+
+<h5>copyright <script language="JavaScript">
+var d=new Date(); 
+yr=d.getFullYear();
+if (yr!=2014)
+document.write(yr);
+</script>.<br/>
+designed by <a href="http://www.deenaneimat.com" target="_blank">deenaneimat.com</a></h5>
+</div>
+
+<div class="clear"></div>
+</div>
+</div>
+<!--end footer-->
+	
+
+	<?php wp_footer(); ?>
+	
+	<!-- Don't forget analytics -->
+    
+    </body>
+
+</html>
--- wp-content/themes/tw/woocommerce/archive-product.php
+++ wp-content/themes/tw/woocommerce/archive-product.php
@@ -90,7 +90,10 @@ get_header( 'shop' ); ?>
 
 	<div id="right">
 	<img src="<?php print get_stylesheet_directory_uri() . '/images/market_hours.png' ?>" class="img-responsive">
-	<p class="market-hours-sidebar">Tuesday-Friday 11am-6pm</p>
+	<p class="market-hours-sidebar">
+		<strong>Online Market Hours:</strong> Wednesday 10 am - Sunday 10 am<br>
+		<strong>Tinwings Walk-in Market:</strong> Tuesday - Friday 11 am - 6 pm
+	</p>
 	<p class="location-sidebar">816 51st Avenue North<br>Nashville, Tennessee</p>
 	<img src="<?php print get_stylesheet_directory_uri() . '/images/my_cart.png' ?>" class="img-responsive">
 	<?php
